75fed665c948c00ea2c6f19a39c2a75288ff9016
nam334 <namratadas334@gmail.com>
2025-05-18 20:14:07 +0530

    solved vertical order Traversal of binary tree

--- Practice Questions/dsarecap/tree/verticalOrderTraversal.js
+++ Practice Questions/dsarecap/tree/verticalOrderTraversal.js
@@ -29,6 +29,43 @@ class VerticalOrderTraversal {
     }
     return result;
   }
+
+  TopViewTraversal(root) {
+    let myMap = new Map(),
+      result = [],
+      queue = [];
+    queue = [[root, 0]];
+    while (queue.length) {
+      let [node, hd] = queue.shift();
+      if (!myMap.has(hd)) myMap.set(hd, [node.data]);
+
+      if (node.left) queue.push([node.left, hd - 1]);
+      if (node.right) queue.push([node.right, hd + 1]);
+    }
+    const keys = Array.from(myMap.keys()).sort((a, b) => a - b);
+    for (let key of keys) {
+      result.push(myMap.get(key));
+    }
+    return result;
+  }
+
+  BottomViewTraversal(root) {
+    let myMap = new Map(),
+      queue = [],
+      result = [];
+    queue = [[root, 0]];
+    while (queue.length) {
+      let [node, hd] = queue.shift();
+      myMap.set(hd, [node.data]);
+      if (node.left) queue.push([node.left, hd - 1]);
+      if (node.right) queue.push([node.right, hd + 1]);
+    }
+    const keys = Array.from(myMap.keys()).sort((a, b) => a - b);
+    for (let key of keys) {
+      result.push(myMap.get(key));
+    }
+    return result;
+  }
 }
 
 const tree = new VerticalOrderTraversal(3);
@@ -38,3 +75,5 @@ tree.root.right.left = new TreeNode(15);
 tree.root.right.right = new TreeNode(7);
 
 console.log(tree.VerticalOrderTraversal(tree.root));
+console.log(tree.TopViewTraversal(tree.root));
+console.log(tree.BottomViewTraversal(tree.root));
